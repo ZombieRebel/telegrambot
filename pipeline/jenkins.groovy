0157996f157520f0920195aa53b65677a26270ea
Rustam Shumnyi <rustam.shumniy@gmail.com>
2023-06-28 14:33:44 +0000

    Add jenkins.groovy

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -1,17 +1,49 @@
 pipeline {
     agent any
     parameters {
-
-        choice(name: 'OS', choices: ['linux', 'darwin', 'windows', 'all'], description: 'Pick OS')
-
+        choice(name: 'OS', choices: ['linux', 'darwin', 'windows'], description: 'Pick target OS')
+        choice(name: 'ARCH', choices: ['arm64', 'amd64'], description: 'Pick target architecture')
+    }
+    environment {
+        REPO = 'https://github.com/zombierebel/telegrambot'
+        BRANCH = 'main'
     }
     stages {
-        stage('Example') {
+        stage('clone') {
             steps {
-                echo "Build for platform ${params.OS}"
+                echo 'Clone repository'
+                git branch: "${BRANCH}", url: "${REPO}"
+            } 
+        }
 
-                echo "Build for arch: ${params.ARCH}"
+        stage('test') {
+            steps {
+                echo 'Test execution started'
+                sh 'make test'
+            }
+        }
 
+        stage('build') {
+            steps {
+                echo echo "Build for platform ${params.OS}, architecture ${params.ARCH}"
+                sh "make build TARGETOS=${params.OS} TARGETARCH=${params.ARCH}"
+            }
+        }
+
+        stage('image') {
+            steps {
+                echo echo "Create an image of build"
+                sh "make image TARGETOS=${params.OS} TARGETARCH=${params.ARCH}"
+            }
+        }
+
+        stage('push') {
+            steps {
+                script {
+                    docker.withRegistry('', 'dockerhub') {
+                        sh 'make push TARGETOS=${params.OS} TARGETARCH=${params.ARCH}'
+                    }
+                }
             }
         }
     }
